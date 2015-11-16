cab38e2ab20d9a002e01010066aae89ac995ae28
Uragan <chikmariov@gmail.com>
2015-11-16 19:18:52 +0200

    Upprize markup

---
+++ Dmitry_Chikmaryov/markup/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        directionNav: true,
+        prevText: " ",
+        nextText: " ",
+        animationLoop: true,
+        itemWidth: 124,
+
+    });
+});
+
